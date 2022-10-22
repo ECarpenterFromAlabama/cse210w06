411b9dd5f8c253ddb05dfb6ad145c81470eec323
ecarpenterfromalabama <u964010154@byui.edu>
2022-10-22 17:57:43 -0500

    Added Retry message to simplify updates

--- wizardsduel/constants.py
+++ wizardsduel/constants.py
@@ -4,3 +4,4 @@
 spell_list = ['Fire', 'Ice', 'Lightning', 'Shield', 'Overkill']
 DEFEAT_MSG = 'You have been defeated!'
 VIC_MSG = 'Your foe has fallen!'
+RETRY_MSG = 'Please select a number value that corresponds to the menu:'
